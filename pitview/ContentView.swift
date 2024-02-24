d98a02baa8d5968fb573832e0c9e4467761ab8fa
Nicolas Guarini <nikiguarincia@gmail.com>
2024-02-24 17:58:43 +0100

    Create SwiftUI Project

---
+++ pitview/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  pitview
+//
+//  Created by Nicolas Guarini on 24/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, PitView!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ pitview/pitviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  pitviewApp.swift
+//  pitview
+//
+//  Created by Nicolas Guarini on 24/02/24.
+//
+
+import SwiftUI
+
+@main
+struct pitviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
